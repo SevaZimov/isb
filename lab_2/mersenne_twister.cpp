dfc5bedc81f86925ce876f36332ef685339d9269
SevaZimov <119681407+SevaZimov@users.noreply.github.com>
2025-05-14 19:17:41 +0400

    added pseudorandom number generator from C++

---
+++ lab_2/mersenne_twister.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <random>
+
+int main() {
+    std::mt19937 generator(666);
+    std::uniform_int_distribution<int> int_dist(0, 1);
+    for (int i = 0; i < 128; i++) {
+        std::cout << int_dist(generator);
+    }
+}
