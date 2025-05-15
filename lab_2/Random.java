d066727eee0276280cb6fde7dc7cfe9ea3364898
Seva <zimovetsva@gmail.com>
2025-05-15 12:51:02 +0400

    added docs to java and C++ randomizers

--- lab_2/Random.java
+++ lab_2/Random.java
@@ -1,6 +1,12 @@
 import java.util.Random;
 
 public class Main {
+    /**
+     * Генерирует и выводит 128-битную псевдослучайную последовательность
+     * используя стандартный генератор Random из Java.
+     * @param args Аргументы командной строки (не используются)
+     * @return void
+     */
     public static void main(String[] args) {
         Random random = new Random();
         byte[] bytes = new byte[16];
--- lab_2/mersenne_twister.cpp
+++ lab_2/mersenne_twister.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
 #include <random>
 
+/**
+ * @brief Генерирует и выводит 128-битную псевдослучайную последовательность
+ *
+ * @details Использует вихрь Мерсенна (Mersenne Twister) в качестве ГПСЧ.
+ * Генерирует последовательность из 128 бит (0 и 1) с равномерным распределением.
+ * @return int Возвращает 0 при успешном выполнении
+ */
+
 int main() {
     std::mt19937 generator(666);
     std::uniform_int_distribution<int> int_dist(0, 1);
